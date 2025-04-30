355eef8bd25f158467c78be9f53ab3c25a822193
parksomii <somi020605@gmail.com>
2025-05-01 01:51:24 +0900

    chore: initial project setup

---
+++ src/main/java/com/blockcloud/BlockcloudApplication.java
@@ -0,0 +1,13 @@
+package com.blockcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blockcloud/BlockcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
