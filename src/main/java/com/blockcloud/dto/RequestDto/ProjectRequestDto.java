b38ec4b48b70b270e067a094f3e431afb8dcfc33
parksomii <somi020605@gmail.com>
2025-08-01 18:12:46 +0900

    feat: Add validation to project name in ProjectRequestDto
    Added @NotBlank annotation to the 'name' field in ProjectRequestDto to ensure that the project name is required. This improves input validation for project creation requests.

--- src/main/java/com/blockcloud/dto/RequestDto/ProjectRequestDto.java
+++ src/main/java/com/blockcloud/dto/RequestDto/ProjectRequestDto.java
@@ -1,5 +1,6 @@
 package com.blockcloud.dto.RequestDto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ProjectRequestDto {
 
+	@NotBlank(message = "프로젝트 이름은 필수입니다.")
 	private String name;
 	private String description;
 }
