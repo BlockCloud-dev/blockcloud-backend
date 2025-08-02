488200d8a1edf378d7c596bd9f286d1bad1dbc1c
parksomii <somi020605@gmail.com>
2025-08-02 19:53:18 +0900

    refactor: simplify CommonResponse structure
    - Removed generic data field and static ok() method
    - Added message field for consistent API responses
    - Prepares structure for standardized success/failure messages

--- src/main/java/com/blockcloud/dto/common/CommonResponse.java
+++ src/main/java/com/blockcloud/dto/common/CommonResponse.java
@@ -1,20 +1,17 @@
 package com.blockcloud.dto.common;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+/**
+ * API 응답의 공통 형식을 제공하는 DTO입니다.
+ * 주로 성공 여부와 메시지를 담아 반환할 때 사용됩니다.
+ */
 @Getter
-@Builder
 @AllArgsConstructor
-public class CommonResponse<T> {
+@NoArgsConstructor
+public class CommonResponse {
 	private boolean success;
-	private T data;
-
-	public static <T> CommonResponse<T> ok(T data) {
-		return CommonResponse.<T>builder()
-			.success(true)
-			.data(data)
-			.build();
-	}
+	private String message;
 }
