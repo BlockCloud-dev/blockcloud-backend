ad7f2668d98092d674facbe83acb95625d3fab80
leeeunda <daun0219@gmail.com>
2025-08-06 22:20:28 +0900

    refactor: move handler file directory

--- src/main/java/com/blockcloud/exception/handler/CustomLogoutSuccessHandler.java
+++ src/main/java/com/blockcloud/exception/handler/CustomLogoutSuccessHandler.java
@@ -1,4 +1,4 @@
-package com.blockcloud.config.handler;
+package com.blockcloud.exception.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
