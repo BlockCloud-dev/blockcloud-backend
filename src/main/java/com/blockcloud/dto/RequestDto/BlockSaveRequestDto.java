7e037aca8da011fe8fc13d60b655d788e6648104
parksomii <somi020605@gmail.com>
2025-08-08 18:39:32 +0900

    feat: Add validation to BlockSaveRequestDto

--- src/main/java/com/blockcloud/dto/RequestDto/BlockSaveRequestDto.java
+++ src/main/java/com/blockcloud/dto/RequestDto/BlockSaveRequestDto.java
@@ -1,18 +1,21 @@
 package com.blockcloud.dto.RequestDto;
 
-import java.time.LocalDateTime;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 블록 저장 요청 DTO
+ * 블록 정보를 포함하여 저장 요청을 처리하기 위한 DTO 클래스입니다.
+ */
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BlockSaveRequestDto {
 
-	private LocalDateTime createdAt;
-	private LocalDateTime updatedAt;
+	@NotNull(message = "블록 정보는 필수입니다.")
 	private Object blocks;
 }
